49ce0d0f59f8c8c3b6c9ed065c6373858d9fca82
kathyleboeuf <kleboeuf@townisp.com>
2018-10-12 17:32:19 -0400

    started queue

---
+++ queueData.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Queue = require('./queue');
+
+function peek(queue) { //works
+	let currentNode = queue.top;
+	if (currentNode === null) {
+		return null;
+	}
+	return currentNode.data
+};
+
+
+const catQueue = () => {
+	cat.enqueue = cat.JSON
+
+
+
+}
